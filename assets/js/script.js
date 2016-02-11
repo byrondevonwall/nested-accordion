9fc61cec5d9b40bf73269851e60d7d75ef6c229a
Byron Wall <byrondevonwall@gmail.com>
2016-02-11 07:41:43 -0500

    Fix arrow styling issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,14 +24,18 @@ toggle1 = !toggle1;
 if(toggle1){
 secOneContent.textContent = "If one examines capitalist objectivism, one is faced with a choice: either accept premodern discourse or conclude that the media is capable of significance. It could be said that any number of narratives concerning Lacanist obscurity may be revealed. Baudrillard uses the term ‘realism’ to denote the bridge between society and sexual identity.";
 arrowOne.className = "fa fa-arrow-down";
+arrowOne.style.color = "dodgerblue";
 secTwoContent.textContent = "";
 arrowTwo.className = "fa fa-arrow-right"
+arrowTwo.style.color = "lightgray";
 secThreeContent.textContent = "";
 arrowThree.className = "fa fa-arrow-right"
+arrowThree.style.color = "lightgray";
 }
 else{
   secOneContent.textContent = "";
   arrowOne.className = "fa fa-arrow-right";
+  arrowOne.style.color = "lightgray";
 }
 }
 );
@@ -44,14 +48,18 @@ showSecTwo.addEventListener("click", function addText2(){
   if(toggle2){
   secTwoContent.textContent = "Therefore, Bataille promotes the use of predialectic narrative to challenge and analyse class. If neosemantic socialism holds, we have to choose between premodern discourse and the capitalist paradigm of narrative.  Thus, Lacan suggests the use of realism to attack class divisions. The main theme of Dietrich’s[2] critique of neosemantic socialism is not theory as such, but neotheory.  It could be said that the subject is interpolated into a premodern discourse that includes art as a paradox. Marx’s model of neosemantic socialism states that sexual identity, somewhat ironically, has significance.";
   arrowTwo.className = "fa fa-arrow-down";
+  arrowTwo.style.color = "dodgerblue";
   secOneContent.textContent = "";
   arrowOne.className = "fa fa-arrow-right";
+  arrowOne.style.color = "lightgray";
   secThreeContent.textContent = "";
   arrowThree.className = "fa fa-arrow-right"
+  arrowThree.style.color = "lightgray";
   }
   else{
     secTwoContent.textContent = "";
     arrowTwo.className = "fa fa-arrow-right"
+    arrowTwo.style.color = "lightgray";
   }
 }
 );
@@ -64,14 +72,18 @@ showSecThree.addEventListener("click", function addText3(){
   if(toggle3){
     secThreeContent.textContent = "In a sense, the subject is contextualised into a premodern discourse that includes culture as a reality. Drucker[5] suggests that we have to choose between realism and prematerialist construction.  It could be said that Debord promotes the use of premodern discourse to read society. The primary theme of Brophy’s[6] essay on realism is the failure, and eventually the futility, of neosemioticist reality.";
     arrowThree.className = "fa fa-arrow-down";
+    arrowThree.style.color = "dodgerblue";
     secOneContent.textContent = "";
     arrowOne.className = "fa fa-arrow-right";
+    arrowOne.style.color = "lightgray";
     secTwoContent.textContent = "";
     arrowTwo.className = "fa fa-arrow-right"
+    arrowTwo.style.color = "lightgray";
   }
   else {
     secThreeContent.textContent = "";
-    arrowThree.className = "fa fa-arrow-right"
+    arrowThree.className = "fa fa-arrow-right";
+    arrowThree.style.color = "lightgray";
   }
   }
 );
