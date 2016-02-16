a5cb9d1b3e11a8931c5d8915aa649cc9e8f005b6
Byron Wall <byrondevonwall@gmail.com>
2016-02-16 17:08:57 -0500

    Add arrow functionality

--- assets/js/single-function-jquery.js
+++ assets/js/single-function-jquery.js
@@ -1,6 +1,3 @@
-// var headings = $(".accordionBody").get(); //this should get all the heading elements.
-// console.log(headings);
-// ​
 $(".wrapper").click(function(e){
   e.preventDefault();
   console.log($(this.child).find(".content"));//this prints out the article tag im looking for.
